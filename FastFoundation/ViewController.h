c7f09f458fd9242ec0263ffb86d42f65ebd3e312
Michael Eisel <michael.eisel@gmail.com>
2017-03-30 20:10:16 -0700

    Initial Commit

---
+++ FastFoundation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastFoundation
+//
+//  Created by Michael Eisel on 3/30/17.
+//  Copyright © 2017 Michael Eisel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
