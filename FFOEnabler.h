b9fd7cd5800422cdb3f5e751e8941f7a0cf6a832
Michael Eisel <michael.eisel@gmail.com>
2018-10-22 11:54:30 -0400

    a go with replacing members of the actual default zone

--- FFOEnabler.h
+++ FFOEnabler.h
@@ -1,6 +1,6 @@
 //Copyright (c) 2018 Michael Eisel. All rights reserved.
 
-#if 1
+#if 0
 #define je_free free
 #define je_malloc malloc
 #define je_realloc realloc
--- FastFoundation/FFOViewController.m
+++ FastFoundation/FFOViewController.m
@@ -112,20 +112,126 @@ void FFOInitialSetup() {
 
 extern char ***_NSGetArgv(void);
 
-extern void je_zone_register(void);
-extern malloc_zone_t *je_get_jemalloc_zone(void);
-
 __attribute__((constructor)) void FFORegister() {
     // je_zone_register();
 }
 
+size_t je_zone_size(malloc_zone_t *zone, const void *ptr);
+void * je_zone_malloc(malloc_zone_t *zone, size_t size);
+void * je_zone_calloc(malloc_zone_t *zone, size_t num, size_t size);
+void * je_zone_valloc(malloc_zone_t *zone, size_t size);
+void je_zone_free(malloc_zone_t *zone, void *ptr);
+void * je_zone_realloc(malloc_zone_t *zone, void *ptr, size_t size);
+void * je_zone_memalign(malloc_zone_t *zone, size_t alignment, size_t size);
+void je_zone_free_definite_size(malloc_zone_t *zone, void *ptr, size_t size);
+void je_zone_destroy(malloc_zone_t *zone);
+unsigned je_zone_batch_malloc(struct _malloc_zone_t *zone, size_t size, void **results, unsigned num_requested);
+void je_zone_batch_free(struct _malloc_zone_t *zone, void **to_be_freed, unsigned num_to_be_freed);
+size_t je_zone_pressure_relief(struct _malloc_zone_t *zone, size_t goal);
+size_t je_zone_good_size(malloc_zone_t *zone, size_t size);
+kern_return_t
+je_zone_enumerator(task_t task, void *data, unsigned type_mask,
+                vm_address_t zone_address, memory_reader_t reader,
+                vm_range_recorder_t recorder);
+boolean_t je_zone_check(malloc_zone_t *zone);
+void je_zone_print(malloc_zone_t *zone, boolean_t verbose);
+void je_zone_log(malloc_zone_t *zone, void *address);
+void je_zone_force_lock(malloc_zone_t *zone);
+void je_zone_force_unlock(malloc_zone_t *zone);
+void je_zone_statistics(malloc_zone_t *zone, malloc_statistics_t *stats);
+boolean_t je_zone_locked(malloc_zone_t *zone);
+void je_zone_reinit_lock(malloc_zone_t *zone);
+
+/*size_t    zone_size(malloc_zone_t *zone, const void *ptr);
+void    *zone_malloc(malloc_zone_t *zone, size_t size);
+void    *zone_calloc(malloc_zone_t *zone, size_t num, size_t size);
+void    *zone_valloc(malloc_zone_t *zone, size_t size);
+void    zone_free(malloc_zone_t *zone, void *ptr);
+void    *zone_realloc(malloc_zone_t *zone, void *ptr, size_t size);
+void    *zone_memalign(malloc_zone_t *zone, size_t alignment,
+                              size_t size);
+void    zone_free_definite_size(malloc_zone_t *zone, void *ptr,
+                                       size_t size);
+void    zone_destroy(malloc_zone_t *zone);
+unsigned    zone_batch_malloc(struct _malloc_zone_t *zone, size_t size,
+                                     void **results, unsigned num_requested);
+void    zone_batch_free(struct _malloc_zone_t *zone,
+                               void **to_be_freed, unsigned num_to_be_freed);
+size_t    zone_pressure_relief(struct _malloc_zone_t *zone, size_t goal);
+size_t    zone_good_size(malloc_zone_t *zone, size_t size);
+kern_return_t    zone_enumerator(task_t task, void *data, unsigned type_mask,
+                                        vm_address_t zone_address, memory_reader_t reader,
+                                        vm_range_recorder_t recorder);
+boolean_t    zone_check(malloc_zone_t *zone);
+void    zone_print(malloc_zone_t *zone, boolean_t verbose);
+void    zone_log(malloc_zone_t *zone, void *address);
+void    zone_force_lock(malloc_zone_t *zone);
+void    zone_force_unlock(malloc_zone_t *zone);
+void    zone_statistics(malloc_zone_t *zone,
+                               malloc_statistics_t *stats);
+static boolean_t    zone_locked(malloc_zone_t *zone);
+static void    zone_reinit_lock(malloc_zone_t *zone);*/
+
+
+
+
+
+static void FFOZoneInit(malloc_zone_t *jemalloc_zone) {
+    malloc_introspection_t jemalloc_zone_introspect = {0};
+    jemalloc_zone_introspect.enumerator = je_zone_enumerator;
+    jemalloc_zone_introspect.good_size = je_zone_good_size;
+    jemalloc_zone_introspect.check = je_zone_check;
+    jemalloc_zone_introspect.print = je_zone_print;
+    jemalloc_zone_introspect.log = je_zone_log;
+    jemalloc_zone_introspect.force_lock = je_zone_force_lock;
+    jemalloc_zone_introspect.force_unlock = je_zone_force_unlock;
+    jemalloc_zone_introspect.statistics = je_zone_statistics;
+    jemalloc_zone_introspect.zone_locked = je_zone_locked;
+    jemalloc_zone_introspect.enable_discharge_checking = NULL;
+    jemalloc_zone_introspect.disable_discharge_checking = NULL;
+    jemalloc_zone_introspect.discharge = NULL;
+#ifdef __BLOCKS__
+    jemalloc_zone_introspect.enumerate_discharged_pointers = NULL;
+#else
+    jemalloc_zone_introspect.enumerate_unavailable_without_blocks = NULL;
+#endif
+    jemalloc_zone_introspect.reinit_lock = je_zone_reinit_lock;
+
+    jemalloc_zone->claimed_address = NULL;
+    jemalloc_zone->size = je_zone_size;
+    jemalloc_zone->free = je_zone_free;
+    jemalloc_zone->batch_free = je_zone_batch_free;
+    jemalloc_zone->realloc = je_zone_realloc;
+    jemalloc_zone->malloc = je_zone_malloc;
+    jemalloc_zone->calloc = je_zone_calloc;
+    jemalloc_zone->valloc = je_zone_valloc;
+    jemalloc_zone->destroy = je_zone_destroy;
+    // jemalloc_zone.zone_name = "jemalloc_zone";
+    jemalloc_zone->batch_malloc = je_zone_batch_malloc;
+    jemalloc_zone->introspect = &jemalloc_zone_introspect;
+    jemalloc_zone->version = 9;
+    jemalloc_zone->memalign = je_zone_memalign;
+    jemalloc_zone->free_definite_size = je_zone_free_definite_size;
+    jemalloc_zone->pressure_relief = je_zone_pressure_relief;
+}
+
+void set_actual_zone(malloc_zone_t *zone);
+
+static malloc_zone_t sOldZone = {0};
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    return;
     je_zone_register();
-    malloc_zone_t *jeZone = je_get_jemalloc_zone();
     malloc_zone_t *defaultZone = malloc_default_zone();
+    sOldZone = *defaultZone;
+    set_actual_zone(&sOldZone);
+    FFOZoneInit(defaultZone);
+    // malloc_zone_t *jeZone = NULL; // je_get_jemalloc_zone();
+    // defaultZone->introspect->force_lock(defaultZone);
+    // printf("%p\n", malloc_zone_malloc(defaultZone, 1));
+    malloc_zone_t *oldDefault = malloc_default_zone();
+
     FFOInitialSetup();
     FFORunTests();
     NSInteger sum = 0;
@@ -171,7 +277,7 @@ __attribute__((constructor)) void FFORegister() {
                 CFTimeInterval end = CACurrentMediaTime();
                 printf("malloc_zone default: %lf\n", (end - start));
             });
-            ({
+            /*({
                 CFTimeInterval start = CACurrentMediaTime();
                 @autoreleasepool {
                     for (NSInteger i = 0; i < nIterations; i++) {
@@ -182,7 +288,7 @@ __attribute__((constructor)) void FFORegister() {
                 }
                 CFTimeInterval end = CACurrentMediaTime();
                 printf("malloc_zone je: %lf\n", (end - start));
-            });
+            });*/
         }
     }
     // if ((rand() & 0) == 1) {
